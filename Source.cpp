7d331b91edf69c266124d002d2ffe4685cee8785
ranaahmed <ahmedyrana63@gmail.com>
2023-03-01 08:52:01 -0800

    hello

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << "Hello my loser team \nI'm sure that we will get bouns \n         good luck";
+	return 0;
+}
